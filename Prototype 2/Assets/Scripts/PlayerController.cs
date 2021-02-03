8394faeda5b1ae1093d92a10ccd24813afebcf75
Kaeli <10856460@my.uvu.edu>
2021-02-03 16:25:01 -0700

    Added Prototype 2 files and started first steps of project.
    Player and Banana move.

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    public float hInput;
+
+    public float xRange = 18;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+    }
+}
