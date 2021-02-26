b607c757a14314a655c5a3660127f0575c79c2dc
Kaeli <10856460@my.uvu.edu>
2021-02-26 09:19:24 -0700

    Did Prototype 2 and Challenge 2. Added files and started on Prototype 3. Have made player move and background move and obstacles to spawn.

---
+++ Prototype 3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    public float jumpforce = 12;
+
+    public float gravityMod = 2;
+
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isOnGround = true;
+    }
+}
