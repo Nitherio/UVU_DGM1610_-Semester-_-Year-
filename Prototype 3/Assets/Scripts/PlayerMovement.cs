e1177321b2dffa27c4c2a3419172d30c12ba1076
Kaeli <10856460@my.uvu.edu>
2021-03-17 17:40:42 -0600

    Fixed jumping bug

--- Prototype 3/Assets/Scripts/PlayerMovement.cs
+++ Prototype 3/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody playerRb;
 
-    public float jumpforce = 12;
+    public float jumpforce = 15;
 
     public float gravityMod = 2;
 
