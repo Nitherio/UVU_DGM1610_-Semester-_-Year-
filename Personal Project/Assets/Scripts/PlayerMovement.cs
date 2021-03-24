fe978dbc1cfd35d21f6f43b62edb0261327808eb
Kaeli <10856460@my.uvu.edu>
2021-03-24 15:54:35 -0600

    Worked on movement for player in personal project

---
+++ Personal Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    public float hInput;
+
+    private float xRange = 17;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
