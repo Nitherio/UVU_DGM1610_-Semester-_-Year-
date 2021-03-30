7568efee2d21a3e2ebd0b018fd0eaab4db848fd3
Kaeli <10856460@my.uvu.edu>
2021-03-30 17:26:41 -0600

    Had to redo all scripts, they wouldn't apply to gameobjects will in a script folder. Characters in place with powerups.

---
+++ Prototype 4/Assets/RotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{   // Variable for rotation speed
+    private float rotationSpeed = 80;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
