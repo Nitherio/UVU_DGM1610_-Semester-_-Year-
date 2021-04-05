fdbca203d7af94da2d185f02f1501aeb55cc34a3
Kaeli <10856460@my.uvu.edu>
2021-04-05 16:52:45 -0600

    Prototype 4 working with enemy bug. Wave numbers is normal now.

--- Prototype 4/Assets/SpawnManager.cs
+++ Prototype 4/Assets/SpawnManager.cs
@@ -7,14 +7,39 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
 
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    public GameObject powerupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Spawn an Enemy
+        SpawnEnemyWave(waveNumber);
 
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
+        // Create Powerup for player to collect
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
-   private Vector3 GenerateSpawnPosition()
+    void Update()
+    {
+        // Find how many enimes are in play
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        // If enemy count is zero, spawn new enemies in greater number
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+
+            // Creates aditional powerups pickups for the player to collect
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+
+
+    private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
@@ -22,4 +47,14 @@ public class SpawnManager : MonoBehaviour
 
         return randomPos;
     }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+
 }
