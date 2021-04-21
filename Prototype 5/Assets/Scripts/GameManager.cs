fb226e0a2ad41464e5b14cd4ba008be2c30560d2
Kaeli <10856460@my.uvu.edu>
2021-04-21 17:23:03 -0600

    Prototype 5.4 and Prototype 5. Can't get the game to play after doing difficulty buttons.

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -21,13 +21,12 @@ public class GameManager : MonoBehaviour
 
     public Button resetButton;
 
+    public GameObject titleScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
-        scoreText.text = "Score: " + score;
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
+
     }
 
     IEnumerator SpawnTarget()
@@ -54,10 +53,19 @@ public class GameManager : MonoBehaviour
         resetButton.gameObject.SetActive(true);
     }
 
-    void ResetGame()
+    public void ResetGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    public void StartGame(int difficulty)
+    {
+        UpdateScore(0);
+        scoreText.text = "Score: " + score;
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+    }
 
 }
